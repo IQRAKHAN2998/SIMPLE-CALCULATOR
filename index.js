94c6a7bc804a48dee681ff80b5e942cd39058592
IQRAKHAN2998 <160261671+IQRAKHAN2998@users.noreply.github.com>
2024-03-21 00:40:28 +0500

    iq-simple-calculator

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
-const answer = await inquirer.prompt([{
+const answer = await inquirer.prompt([
+    {
         message: "enter first number",
         type: "number",
-        name: "firstNumber"
+        name: "firstNumber",
     },
-    { message: "enter second number",
-        type: "number",
-        name: "secondNumber" },
-    { message: "select one of the operator to perfom operation",
+    { message: "enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of the operator to perfom operation",
         type: "list",
         name: "operator",
-        choices: ["addition", "subtraction", "multiplication", "division"], },
+        choices: [
+            "addition",
+            "subtraction",
+            "multiplication",
+            "division",
+            "module",
+            "exponent",
+        ],
+    },
 ]);
 console.log(answer);
 //conditional statement
@@ -25,6 +34,12 @@ else if (answer.operator === "multiplication") {
 }
 else if (answer.operator === "division") {
     console.log(answer.firstNumber / answer.secondNumber);
+}
+else if (answer.operator === "module") {
+    console.log(answer.firstNumber % answer.secondNumber);
+}
+else if (answer.operator === "exponent") {
+    console.log(answer.firstNumber ** answer.secondNumber);
 }
 else {
     console.log("please select valid operator");
--- index.ts
+++ index.ts
@@ -1,31 +1,41 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
-const answer = await inquirer.prompt([{
-message : "enter first number",
-type : "number",
-name: "firstNumber"},
-{message : "enter second number",
-type : "number",
-name: "secondNumber"},
-{message : "select one of the operator to perfom operation",
-type : "list",
-name: "operator",
-choices : ["addition" , "subtraction","multiplication","division"],},
+const answer = await inquirer.prompt([
+  {
+    message: "enter first number",
+    type: "number",
+    name: "firstNumber",
+  },
+  { message: "enter second number", type: "number", name: "secondNumber" },
+  {
+    message: "select one of the operator to perfom operation",
+    type: "list",
+    name: "operator",
+    choices: [
+      "addition",
+      "subtraction",
+      "multiplication",
+      "division",
+      "module",
+      "exponent",
+    ],
+  },
 ]);
 console.log(answer);
 
 //conditional statement
-if(answer.operator === "addition"){
-    console.log(answer.firstNumber+ answer.secondNumber);
-}
-else if(answer.operator === "subtraction"){
-    console.log(answer.firstNumber - answer.secondNumber);
-}
-else if(answer.operator === "multiplication"){
-    console.log(answer.firstNumber * answer.secondNumber);
-}
-else if(answer.operator === "division"){
-    console.log(answer.firstNumber / answer.secondNumber);
-}
-else{
-    console.log("please select valid operator")
+if (answer.operator === "addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else if (answer.operator === "module") {
+    console.log(answer.firstNumber % answer.secondNumber);
+} else if (answer.operator === "exponent") {
+    console.log(answer.firstNumber ** answer.secondNumber);
+} else {
+  console.log("please select valid operator");
 }
